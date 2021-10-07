22a9003a3257aab44fbc32b504e21539ff037ea9
Niraj Patel <90847730+NirajPatel2021@users.noreply.github.com>
2021-10-06 22:24:37 -0500

    lab_2

---
+++ lab_2/src/main/java/edu/ben/controllers/FormHandler.java
@@ -0,0 +1,27 @@
+package edu.ben.controllers;
+
+import java.io.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+
+@WebServlet(name = "FormHandler", value = "/FormHandler")
+public class FormHandler extends HttpServlet {
+    private String message;
+
+    public void init() {
+        message = "Hello World!";
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+
+        // Hello
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>" + message + "</h1>");
+        out.println("</body></html>");
+    }
+
+    public void destroy() {
+    }
+}
